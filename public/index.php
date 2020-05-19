c58595b042d4cf1b4170a7ffe5c60084b72e494e
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-05-19 14:03:49 +0300

    use PSR-4 autoload

--- public/index.php
+++ public/index.php
@@ -2,9 +2,8 @@
 declare(strict_types=1);
 
 require_once __ROOT__ . '/src/utils/include.php';
-require_once __ROOT__ . '/src/dto/album.php';
 
-use Somatik\Album;
+use App\Somatik\Album;
 
 $albums = Album::getAll();
 
